a4af03d17b8e616fa7c6042b16a703237cef7aaa
Ubuntu <ubuntu@ip-172-31-45-42.us-east-2.compute.internal>
2025-08-20 04:55:38 +0000

    filename

--- process_gambling/etl.py
+++ process_gambling/etl.py
@@ -5,7 +5,7 @@ def run(sport: str):
     api = Etl(sport=sport)
     df = api.download_sports()
     api.load(df, 'BRONZE_ODDSAPI_SPORTS')
-    api.save_to_s3(f'data/{api.DB_NAME}_{api.DB_VERSION}', f'code/process_gambling/data/{api.DB_NAME}_{api.DB_VERSION}.db')
+    api.save_to_s3(f'data/{api.DB_NAME}_{api.DB_VERSION}.db', f'code/process_gambling/data/{api.DB_NAME}_{api.DB_VERSION}.db')
 
 
 if __name__ == '__main__':
