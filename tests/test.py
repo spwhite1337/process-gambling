3f2bb058e59193b4d1a1a2cb49b74d56d2f5a1f9
Scott White <spwhite1337@gmail.com>
2025-11-22 20:30:44 +0000

    test

--- tests/test.py
+++ tests/test.py
@@ -6,3 +6,9 @@ class Tests(unittest.TestCase):
         import boto3
         client = boto3.client('s3')
 
+    def test_model(self):
+        from process_gambling.model.etl import Etl
+        mdl = Etl(sport='americanfootball_nfl', version='v0')
+        df = mdl.download()
+        print(df.shape)
+
