05611d6bbc33f122a7abcdfe458e2ce23a904e4e
Scott White <spwhite1337@gmail.com>
2025-11-23 15:13:38 +0000

    drop na

--- process_gambling/model/wrangle.py
+++ process_gambling/model/wrangle.py
@@ -8,15 +8,15 @@ from process_gambling.model.etl import Etl
 class Wrangle(Etl):
 
     def _transform(self, df: pd.DataFrame) -> pd.DataFrame:
+        df = df[self.GROUP_COLS + self.features + [self.response_col]]
         # Drop na
         df = df.dropna()
         return df
 
     def fit_transform(self, df: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
-        df = df[self.GROUP_COLS + self.features + [self.response_col]]
         # Maybe drop 2020 and prior due to different game? Maybe time filter?
-        # df_all = df_all[df_all['season'] > 2020]
-
+        # df = df[df['season'] > 2020]
+        df = self._transform(df)
         df_train = df[df['season'] < self.model_params['val_year']].copy()
         df_test = df[df['season'] == self.model_params['val_year']].copy()
         return df_train, df_test
--- tests/test.py
+++ tests/test.py
@@ -11,5 +11,6 @@ class Tests(unittest.TestCase):
         mdl = Train(sport='americanfootball_nfl', version='v0')
         df = mdl.download()
         df_train, df_test = mdl.fit_transform(df)
+        print(df.isna().mean())
         mdl.train(df_train)
 
