99ed0a4818d53136b10d7ba60e1b70dbf3fefe8d
Scott White <spwhite1337@gmail.com>
2025-11-25 21:24:17 +0000

    WIP

--- process_gambling/model/validate.py
+++ process_gambling/model/validate.py
@@ -22,13 +22,14 @@ class Validate(Train):
         if self.ptiles is None:
             self.ptiles = np.percentile(df_val['preds'].values, np.arange(0, 100, 11))
 
+        null_hyp = df_val['trues'].mean()
         for th in self.ptiles:
-            wins = (1-df_val[df_val['preds'] <= th]['trues']).sum()
-            losses = (df_val[df_val['preds'] <= th]['trues']).sum()
+            wins = (df_val[df_val['preds'] >= th]['trues']).sum()
+            losses = (1-df_val[df_val['preds'] >= th]['trues']).sum()
             if wins+losses == 0:
                 continue
-            pvalue = binomtest(wins, wins+losses, 0.5).pvalue
-            print(f'th: {round(th, 3)}, {wins}, {losses}, {round(wins/(wins+losses), 3)}, pvalue: {round(pvalue, 2)}')
+            pvalue = binomtest(wins, wins+losses, null_hyp).pvalue
+            print(f'th: {round(th, 3)}, {wins}, {losses}, {round(wins/(wins+losses), 3)}, pvalue: {round(pvalue, 3)}')
 
         if table_name is not None:
             self.upload(df_val, table_name)
