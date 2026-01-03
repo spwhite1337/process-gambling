08fcc6e245ba3e67aed6a79629c4f3ff44e6a3c0
Scott White <spwhite1337@gmail.com>
2026-01-03 19:05:19 +0000

    service unavailable catcher

--- process_gambling/etl.py
+++ process_gambling/etl.py
@@ -47,11 +47,11 @@ class Run(Etl):
         for table_to_append in tables_to_append:
             # Run append command
             cursor.execute(f"""
-            CREATE TABLE {table_to_append}_test AS (
+            CREATE TABLE {table_to_append}_test AS 
                 SELECT * FROM {table_to_append}
                 UNION ALL
                 SELECT * FROM {table_to_append}_UPDATE
-            );
+            ;
             """)
             # Drop update table
             # cursor.execute(f'DROP TABLE {table_to_append}_UPDATE;')
